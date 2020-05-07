97d490a763e66092c0e52db0a8e488809d3dfae5
prudhvin-nagareddy <prudhvin.nagareddy@atmecs.com>
2020-05-07 15:18:07 +0530

    Modified Sceario2 outputs

--- Scenario2_Nat-Instance/outputs.tf
+++ Scenario2_Nat-Instance/outputs.tf
@@ -1,6 +1,18 @@
-#output "address" {
- # value = aws_elb.web.dns_name
-#}
+output "Webserver1_ip" {
+  value = "${aws_instance.DevOps-WebServer1.public_ip}"
+}
+
+output "Webserver2_ip" {
+  value = "${aws_instance.DevOps-WebServer2.public_ip}"
+}
+
+output "Appserver1_ip" {
+  value = "${aws_instance.DevOps-AppServer1.private_ip}"
+}
+
+output "Appserver2_ip" {
+  value = "${aws_instance.DevOps-AppServer2.private_ip}"
+}
 
 output "nat_instance_id" {
  value = "${aws_instance.DevOps-Nat.id}"
